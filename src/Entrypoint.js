2f87180697fb5ac77d05cdcf017fa53b825329d5
ikukojohanna <johanna.odersky@gmail.com>
2023-03-03 23:15:06 +0100

    Added GLTF IkuCharacter

--- src/Entrypoint.js
+++ src/Entrypoint.js
@@ -1,11 +1,16 @@
 import React, { useRef } from "react";
 import "./styles.css";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Sparkles, PositionalAudio } from "@react-three/drei";
+import {
+    OrbitControls,
+    useGLTF,
+    Sparkles,
+    PositionalAudio,
+} from "@react-three/drei";
+import * as THREE from "three";
 
 import { CircleGeometry, Mesh, MeshBasicMaterial } from "three";
 
-import { Text } from "@react-three/drei";
 import {
     EffectComposer,
     HueSaturation,
@@ -22,14 +27,66 @@ let mesh = new Mesh(
     new MeshBasicMaterial({
         color: 0xf64226,
         transparent: true,
-        opacity: 0.2,
+        opacity: 1,
         position: [5, 5, 5],
+        side: THREE.DoubleSide,
     })
 );
-
 mesh.rotation.x = Math.PI * 0.5;
 mesh.position.set(1.17, 10.7, -4.1);
 mesh.scale.set(1.5, 1, 1);
+
+export function Model5(props) {
+    const { nodes, materials } = useGLTF("/ikuwhiteflatmerged.glb");
+    return (
+        <group {...props} dispose={null}>
+            <group
+                position={[0, -0.7, 0.01]}
+                rotation={[0, 0, 0]}
+                scale={0.0017}
+            >
+                <mesh
+                    geometry={nodes.Merged_Objects.geometry}
+                    material={materials.Bilbao}
+                />
+            </group>
+        </group>
+    );
+}
+export function Model8(props) {
+    const { nodes, materials } = useGLTF("/ikuwhiteflatmerged.glb");
+    return (
+        <group {...props} dispose={null}>
+            <group
+                position={[0, -0.7, -0.01]}
+                rotation={[0, 0, 0]}
+                scale={0.0017}
+            >
+                <mesh
+                    geometry={nodes.Merged_Objects.geometry}
+                    material={materials.Bilbao}
+                />
+            </group>
+        </group>
+    );
+}
+export function Model6(props) {
+    const { nodes, materials } = useGLTF("/ikublack2.glb");
+    return (
+        <group
+            position={[0, -0.7, 0]}
+            rotation={[0, 0, 0]}
+            scale={0.0017}
+            {...props}
+            dispose={null}
+        >
+            <mesh
+                geometry={nodes.Merged_Objects.geometry}
+                material={materials.Bilbao}
+            />
+        </group>
+    );
+}
 
 function SceneParticles() {
     return (
@@ -80,62 +137,147 @@ function SceneParticles() {
         </>
     );
 }
-function TextIku() {
+export function Stora(props) {
+    const { nodes, materials } = useGLTF("/stora.glb");
     return (
-        <>
-            <Text
-                anchorX="center" // default
-                anchorY="middle" // default
-                rotation={[0, 0, 0]}
-                position={[0.7, 1, 1]}
-                fontSize={0.8}
-                color="black"
-            >
-                Iku
-            </Text>
-            <Text
-                color="black"
-                anchorX="center" // default
-                anchorY="middle" // default
-                rotation={[0, 0, 0]}
-                position={[0.8, 0.3, 1]}
-                fontSize={0.9}
-            >
-                Itch
-            </Text>
-            <Text
-                color="black"
-                anchorX="center" // default
-                anchorY="middle" // default
-                rotation={[0, 0, 0]}
-                position={[-0.1, 0.6, 1]}
-                fontSize={0.2}
-            >
-                The
-            </Text>
-            <Text
-                color="white"
-                anchorX="center" // default
-                anchorY="middle" // default
-                rotation={[0, 0, 0]}
-                position={[0, -1, 1]}
-                fontSize={0.12}
-            >
-                14.04.2023
-            </Text>
-        </>
+        <group {...props} dispose={null}>
+            <group rotation={[-1.5, 0, 0]} scale={0.012} position={[0, 0, 1]}>
+                <mesh
+                    geometry={nodes["3D_Text_-_I"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[0, 0, 75]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_K"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[20, 0, 75]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_U"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[60, 0, 75]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_T"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[-40, 0, 67]}
+                    scale={0.33}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_H"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[-27, 0, 67]}
+                    scale={0.3}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_E"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[-14, 0, 67]}
+                    scale={0.3}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_I"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[0, 0, 37]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_T_1"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[17, 0, 37]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_C"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[50, 0, 37]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_H_1"].geometry}
+                    material={materials["Historic Shade"]}
+                    position={[90, 0, 37]}
+                />
+            </group>
+        </group>
+    );
+}
+
+export function Date(props) {
+    const { nodes, materials } = useGLTF("/date.glb");
+    return (
+        <group {...props} dispose={null} rotation={[-1.5, 0, -3.15]}>
+            <group scale={0.012} position={[0, -0.1, -0.2]}>
+                <mesh
+                    geometry={nodes["3D_Text_-_1"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[-147.38, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_4"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[-118.58, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_4_1"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[-36.05, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[-85.95, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-__1"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[-3.42, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_0"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[-71.1, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_0_1"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[43.96, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_2"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[11.43, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_2_1"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[79.02, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+                <mesh
+                    geometry={nodes["3D_Text_-_3"].geometry}
+                    material={materials["City Terrace"]}
+                    position={[111.55, 0, 0]}
+                    scale={[1, 0.13, 1]}
+                />
+            </group>
+        </group>
     );
 }
 
 function Circle() {
     const mesh = useRef();
-    //useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
     return (
         <mesh ref={mesh} position={[0, 0, -0.01]}>
-            <circleGeometry args={[1.9, 48]} />
+            <circleGeometry args={[1.7, 48]} />
             <PositionalAudio
-                position={[0, 0, 5]}
+                position={[0, 0, 0]}
                 autoplay
                 loop
                 url="itchwebsite.wav"
@@ -146,7 +288,7 @@ function Circle() {
                 attach="material"
                 color={"white"}
                 transparent={true}
-                opacity={0}
+                opacity={0.1}
             />
         </mesh>
     );
@@ -154,12 +296,20 @@ function Circle() {
 
 export default function EntryPoint() {
     return (
-        <Canvas camera={{ fov: 50, position: [0, 0, 6] }}>
-            <OrbitControls maxPolarAngle={Math.PI / 2} minPolarAngle={0} />
+        <Canvas camera={{ fov: 40, position: [0, 0, 7] }}>
+            <OrbitControls
+                maxPolarAngle={Math.PI / 2}
+                minPolarAngle={Math.PI / 2}
+            />
             <Circle />
-            <TextIku />
-            <SceneParticles />
+            <Stora />
+            <Date />
+            <Model5 />
+            <Model6 />
+            <Model8 />
 
+            <SceneParticles />
+            <ambientLight />
             <EffectComposer stencilBuffer={true}>
                 <DepthOfField
                     focusDistance={0.012}
@@ -175,12 +325,12 @@ export default function EntryPoint() {
                 <GodRays
                     sun={mesh}
                     blendFunction={BlendFunction.Screen}
-                    samples={150}
-                    density={1}
-                    decay={0.94}
+                    samples={120}
+                    density={0.8}
+                    decay={0.9}
                     weight={5}
-                    exposure={0.2}
-                    clampMax={0.7}
+                    exposure={0.08}
+                    clampMax={0.8}
                     width={Resizer.AUTO_SIZE}
                     height={Resizer.AUTO_SIZE}
                     kernelSize={KernelSize.SMALL}
